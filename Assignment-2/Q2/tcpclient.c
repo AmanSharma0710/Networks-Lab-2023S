807325c0a86c70bfb985ef44a72c2cbacfdc3089
Aman Sharma <sharmaaman2050as@gmail.com>
2023-01-18 23:20:31 +0530

    Finished coding Q2. Known bugs: cd ..; pwd doesn't work, pressing enter directly is not handled.

--- Assignment-2/Q2/tcpclient.c
+++ Assignment-2/Q2/tcpclient.c
@@ -11,13 +11,33 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+void send_in_packets(int sockfd, char *data, size_t data_len){
+	if(data_len == 0) return;
+	printf("Sending: %s\n", data);
+	size_t packet_len = 50;
+	char *packet = (char *)malloc(packet_len*sizeof(char));
+	int sent = 0;
+	while(sent < data_len){
+		int i;
+		for(i=0; i<packet_len; i++){
+			if(i+sent < data_len){
+				packet[i] = data[i+sent];
+			}else{
+				packet[i] = '\0';
+			}
+		}
+		send(sockfd, packet, packet_len, 0);
+		sent += packet_len;
+	}
+	free(packet);
+}
+
 int main()
 {
 	int			sockfd ;
 	struct sockaddr_in	serv_addr;
 
 	int i;
-	char buf[50];
 
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("Unable to create socket\n");
@@ -34,27 +54,38 @@ int main()
 		exit(0);
 	}
 
-	for(i=0; i < 50; i++) buf[i] = '\0';
-	recv(sockfd, buf, 50, 0);
+	size_t buf_len = 50;
+	char *buf = (char *)malloc(buf_len*sizeof(char));
+	for(i=0; i < buf_len; i++) buf[i] = '\0';
+	recv(sockfd, buf, buf_len, 0);
 	if(strcmp(buf, "LOGIN:") != 0){
 		printf("Unrecognised server response\n");
 		close(sockfd);
 		exit(0);
 	}
 	printf("%s ", buf);
-	getline(&buf, 50, stdin);
-	int input_length = strlen(buf);
+	size_t input_length = 10;
+	char *input = (char *)malloc(input_length*sizeof(char));
+	getline(&input, &input_length, stdin);
 	if(input_length>26){	//25 + 1 for newline
-		printf("Input too long. Please try again.\n");
+		printf("Invalid username\n");
 		close(sockfd);
 		exit(0);
 	}
-	for(int i=input_length-1; i<50; i++){	//Overwrite the newline with nulls
-		buf[i] = '\0';
+	//Copy the input into the buffer, padding with \0 if the input is less than 50 bytes
+	for(i=0; i<buf_len; i++){
+		if(i<input_length-1){
+			buf[i] = input[i];
+		}else{
+			buf[i] = '\0';
+		}
 	}
+	free(input);
+	//Send the username to the server
 	send(sockfd, buf, 50, 0);
 	for(i=0; i < 50; i++) buf[i] = '\0';
 	recv(sockfd, buf, 50, 0);
+	//If the server sends NOT-FOUND, the username is invalid
 	if(strcmp(buf, "NOT-FOUND") == 0){
 		printf("Invalid username\n");
 		close(sockfd);
@@ -67,19 +98,18 @@ int main()
 	}
 	while(1){
 		printf("Enter a command to be executed on the server(pwd, dir, cd, exit): ");
-		for(i=0; i < 50; i++) buf[i] = '\0';
-		getline(&buf, 50, stdin);
-		input_length = strlen(buf);
-		buf[--input_length] = '\0';	//Remove the newline
-		if(input_length>50){
-			printf("Input too long. Please try again.\n");
-			continue;
-		}
-
-		send(sockfd, buf, 50, 0);
+		input_length = 10;
+		input = (char *)malloc(input_length*sizeof(char));
+		//Clear the buffer and receive the input from the user
+		for(i=0; i < input_length; i++) input[i] = '\0';
+		getline(&input, &input_length, stdin);
+		input_length = strlen(input);
+		input[--input_length] = '\0';	//Remove the newline character from the input
+		send_in_packets(sockfd, input, input_length);
+		printf("%s\n", input);
 
 		//If the user enters exit, close the socket and exit without waiting for a response from the server
-		if(strcmp(buf, "exit") == 0){
+		if(strcmp(input, "exit") == 0){
 			close(sockfd);
 			exit(0);
 		}
@@ -102,30 +132,23 @@ int main()
 			}
 		}
 		//Receive response from server in packets of 50 bytes until the server sends \0
-		if(response_end == 0){
-			while(1){
-				//Clear the buffer and receive the next packet
-				for(i=0; i < 50; i++) buf[i] = '\0';
-				recv(sockfd, buf, 50, 0);
+		while(response_end == 0){
+			//Clear the buffer and receive the next packet
+			for(i=0; i < 50; i++) buf[i] = '\0';
+			recv(sockfd, buf, 50, 0);
 
-				//If the server sends \0, set the break flag to 1
-				int break_flag = 0;
-				for(int i=0; i<50; i++){
-					if(buf[i] == '\0'){
-						break_flag = 1;
-						break;
-					}
-				}
-				//Print the received packet
-				printf("%s", buf);
-
-				//If the server sent \0, break out of the loop
-				if(break_flag == 1){
-					printf("\n");
+			//If the server sends \0, set the response_end flag to 1
+			for(int i=0; i<50; i++){
+				if(buf[i] == '\0'){
+					response_end = 1;
 					break;
 				}
 			}
+			//Print the received packet
+			printf("%s", buf);
 		}
+		printf("\n");
+		free(input);
 	}
 	close(sockfd);
 	return 0;
--- Assignment-2/Q2/tcpserver_conc.c
+++ Assignment-2/Q2/tcpserver_conc.c
@@ -2,14 +2,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <dirent.h>
 #include <sys/types.h>
 #include <sys/socket.h> 
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+void send_in_packets(int sockfd, char *data, size_t data_len){
+	size_t packet_len = 50;
+	char *packet = (char *)malloc(packet_len*sizeof(char));
+	int sent = 0;
+	while(sent < data_len){
+		int i;
+		for(i=0; i<packet_len; i++){
+			if(i+sent < data_len){
+				packet[i] = data[i+sent];
+			}else{
+				packet[i] = '\0';
+			}
+		}
+		send(sockfd, packet, packet_len, 0);
+		sent += packet_len;
+	}
+	free(packet);
+}
 
-int main()
-{
+
+int main(){
 	int			sockfd, newsockfd ; /* Socket descriptors */
 	int			clilen;
 	struct sockaddr_in	cli_addr, serv_addr;
@@ -72,11 +91,13 @@ int main()
 			}
 			fclose(fp);
 
-			//Clear the buffer and send the result
+			//Clear the buffer and send the buffer
 			for(i=0; i < 50; i++) buf[i] = '\0';
 			if(found == 0){
 				strcpy(buf, "NOT-FOUND");
 				send(newsockfd, buf, strlen(buf) + 1, 0);
+				close(newsockfd);
+				exit(0);
 			}
 			else{
 				strcpy(buf, "FOUND");
@@ -85,25 +106,133 @@ int main()
 
 			//We now receive commands from the client that we will execute
 			while(1){
-				for(i=0; i < 50; i++) buf[i] = '\0';
-				recv(newsockfd, buf, 50, 0);
-				if(strcmp(buf, "exit") == 0){
-					break;
+				size_t len = 1024;
+				char *local_buffer = (char *)malloc(len*sizeof(char));
+
+				//Keep receiving until we get a null character, and add them to local_buffer
+				int response_end = 0, local_buffer_filled=0;
+				while(response_end == 0){
+					recv(newsockfd, buf, 50, 0);
+					for(i=0; i < 50; i++){
+						local_buffer[local_buffer_filled++] = buf[i];
+						if(local_buffer_filled == len){
+							printf("Buffer overflow\n");
+							exit(0);
+						}
+						if(buf[i] == '\0'){
+							response_end = 1;
+							break;
+						}
+						buf[i] = '\0';
+					}
 				}
-				if(getcwd(buf, 50) == NULL){
-					strcpy(buf, "ERROR");
+				//Tokenize the command
+				char* token = strtok(local_buffer, " ");
+				char **tokens = (char **)malloc(3*sizeof(char *));
+				int token_count = 0;
+				for(int i=0; i<3; i++){
+					if(token == NULL){
+						break;
+					}
+					tokens[i] = (char *)malloc(200*sizeof(char));
+					strcpy(tokens[i], token);
+					token_count++;
+					token = strtok(NULL, " ");
+				}
+				printf("Token count: %d\n", token_count);
+				for(int i=0; i<token_count; i++){
+					printf("Token %d: %s\n", i, tokens[i]);
+				}
+				//Clear the local_buffer
+				for(i=0; i<1024; i++) local_buffer[i] = '\0';
+
+				//If token_count is 0, it is an empty command and we return empty string
+				if(token_count == 0){
+					strcpy(buf, "\0");
 					send(newsockfd, buf, strlen(buf) + 1, 0);
+					free(tokens);
+					free(local_buffer);
+					continue;
 				}
-				else{
-					send(newsockfd, buf, strlen(buf) + 1, 0);
+
+				//Execute the command
+				if((strcmp(tokens[0], "exit") == 0)&&(token_count == 1)){
+					break;	//Exit the loop
 				}
+				if((strcmp(tokens[0], "pwd") == 0)&&(token_count == 1)){
+					printf("Executing pwd command\n");	
+					if(getcwd(local_buffer, len) == NULL){	//If we get an error
+						strcpy(buf, "####");
+						send(newsockfd, buf, strlen(buf) + 1, 0);
+					}
+					else{	//If we get a valid response
+						send_in_packets(newsockfd, local_buffer, strlen(local_buffer));
+					}
+					printf("Done executing pwd command\n");
+					free(tokens);
+					free(local_buffer);
+					continue;
+				}
+				if((strcmp(tokens[0], "dir") == 0)&&(token_count == 1 || token_count == 2)){
+					DIR *d;
+					struct dirent *dir;
+					if(token_count == 2){
+						d = opendir(tokens[1]);
+					}
+					else if(token_count == 1){
+						d = opendir(".");
+					}
+					if(d){
+						while((dir = readdir(d)) != NULL){
+							strcat(local_buffer, dir->d_name);
+							strcat(local_buffer, "\t");
+						}
+						closedir(d);
+						send_in_packets(newsockfd, local_buffer, strlen(local_buffer));
+					}
+					else{
+						strcpy(buf, "####");
+						send(newsockfd, buf, strlen(buf) + 1, 0);
+					}
+					free(tokens);
+					free(local_buffer);
+					continue;
+				}
+				if((strcmp(tokens[0], "cd") == 0)&&(token_count == 1 || token_count == 2)){
+					if(token_count == 1 || strcmp(tokens[1], "~") == 0){
+						if(tokens[1] == NULL)
+							tokens[1] = (char *)malloc(200*sizeof(char));
+						strcpy(tokens[1], "~/");
+					}
+					if(tokens[1][0] == '~'){
+						char *home = getenv("HOME");
+						char *temp = (char *)malloc(200*sizeof(char));
+						strcpy(temp, home);
+						strcat(temp, tokens[1] + 1);
+						strcpy(tokens[1], temp);	//Buffer overflow?
+						free(temp);
+					}
+					if(chdir(tokens[1]) == -1){	//If we get an error
+						strcpy(buf, "####");
+						send(newsockfd, buf, strlen(buf) + 1, 0);
+					}
+					else{	//If we get a valid response
+						strcpy(buf, "Working Directory Changed");
+						send(newsockfd, buf, strlen(buf) + 1, 0);
+					}
+					free(tokens);
+					free(local_buffer);
+					continue;
+				}
+				//If we reach here, the command is invalid
+				strcpy(buf, "$$$$");
+				send(newsockfd, buf, strlen(buf) + 1, 0);
+				free(tokens);
+				free(local_buffer);
 			}
-
-
 			close(newsockfd);
 			exit(0);
 		}
-
 		close(newsockfd);
 	}
 	return 0;
