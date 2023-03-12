34f32a28d8fa3a21ec99f48ad3e89caf4d4add74
arekhani10 <arekhani5@gmail.com>
2023-03-13 00:14:10 +0530

    Assignment 5 init

---
+++ Assignment-5/mysocket.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<poll.h> 
+#include<string.h>
+#include<sys/types.h>
+#include<time.h>
+#include<sys/socket.h>
+#include <arpa/inet.h>
+#include<netinet/in.h>
+#include<ctype.h>
+
+
