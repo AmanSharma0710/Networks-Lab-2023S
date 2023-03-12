605af07885ed3cde20389ef9a7f68686a2e85e39
arekhani10 <arekhani5@gmail.com>
2023-03-13 00:33:09 +0530

    header file init

--- Assignment-5/mysocket.h
+++ Assignment-5/mysocket.h
@@ -10,4 +10,14 @@
 #include<netinet/in.h>
 #include<ctype.h>
 
+int my_socket(int domain, int type, int protocol);
 
+int my_bind(int sockfd, const struct sockaddr* addr, socklen_t addrlen);
+
+int my_connect(int sockfd, const struct sockaddr* addr, socklen_t addrlen);
+
+int my_accept(int sockfd, struct sockaddr *restrict addr, socklen_t *restrict addrlen);
+
+int my_listen(int sockfd, int backlog);
+
+int my_close(int sockfd);
